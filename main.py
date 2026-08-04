65acc2ca6640caf097dcfff0a4d30e2bd1f8ca24
Overmod17 <123484614+Overmod17@users.noreply.github.com>
2026-08-04 02:54:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print("Hola")
+import os
+import requests
+import pandas as pd
+from io import StringIO
+SHEET_URL = os.environ["SHEET_URL"]
+response = requests.get(SHEET_URL)
+response.raise_for_status()
+csv_data = StringIO(response.text)
+df = pd.read_csv(csv_data)
+print(df)
+print()
+print("Filas:", len(df))
+print("Columnas:", len(df.columns))
